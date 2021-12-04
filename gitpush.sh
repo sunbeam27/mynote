24ec01d3346a10b5e58ee42122eb93fcb5e62fa5
sunmeng <1241577140@qq.com>
2021-12-04 22:26:07 +0800

    Update

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,7 @@
+
 git pull origin main
 git config --global user.name "sunmeng"
 git config --global user.email "1241577140@qq.com"
 git add .
 git commit -m "Update"
-git push sunmeng HEAD
+git push 
